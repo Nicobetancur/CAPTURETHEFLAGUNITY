1c0c5e5fc498a8f83d5720376327f1b5bf74af45
Nicobetancur <nicobetancur@ufl.edu>
2020-10-20 00:52:56 -0400

    second commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+    public float radius = 3f;
+    bool isFocus = false;
+    bool hasInteracted = false;
+    Transform player;
+    public Transform SPAWN;
+
+
+    public virtual void Interact()
+    {
+        //This method is meant to be overwritten
+        UnityEngine.Debug.Log("Interacting with " + transform.name);
+        
+        if (transform.name == "Player")
+        {
+
+            transform.position = SPAWN.position;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, transform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+
+    }
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
